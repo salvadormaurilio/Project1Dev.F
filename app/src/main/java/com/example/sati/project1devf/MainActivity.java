cfcb6474bf58301daca37ab7b2e4fc0202cccb15
Salvador <salvadoredgar99@gmail.com>
2015-06-19 19:51:33 -0500

    Add login alyout with RelativeLayout

--- app/src/main/java/com/example/sati/project1devf/MainActivity.java
+++ app/src/main/java/com/example/sati/project1devf/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.sati.project1devf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login_linear_layouts);
     }
 }
